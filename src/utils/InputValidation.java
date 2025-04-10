937afcdb7a9f1d964cea243a4d10080ad4e3798c
Ahmed Saif <asaif@sumerge.com>
2025-04-10 17:12:54 +0200

    refactor: replace hardcoded validation message with a variable

--- src/utils/InputValidation.java
+++ src/utils/InputValidation.java
@@ -3,29 +3,28 @@ package utils;
 import java.util.Scanner;
 
 public class InputValidation {
+    private static String message = "Invalid input. Please try again.";
+    private static Scanner scanner = new Scanner(System.in);
     public static int validate(int min, int max) {
-        Scanner scanner = new Scanner(System.in);
         int choice = scanner.nextInt();
         while (choice < min || choice > max) {
-            System.out.println("Invalid choice. Please try again.");
+            System.out.println(message);
             choice = scanner.nextInt();
         }
         return choice;
     }
     public static char validate(char start,char end) {
-        Scanner scanner = new Scanner(System.in);
         char choice = scanner.next().charAt(0);
         while (choice < start || choice > end) {
-            System.out.println("Invalid choice. Please try again.");
+            System.out.println(message);
             choice = scanner.next().charAt(0);
         }
         return choice;
     }
     public static char validateYesNo() {
-        Scanner scanner = new Scanner(System.in);
         char choice = scanner.next().charAt(0);
         while (choice != 'y' && choice != 'n') {
-            System.out.println("Invalid choice. Please try again.");
+            System.out.println(message);
             choice = scanner.next().charAt(0);
         }
         return choice;
